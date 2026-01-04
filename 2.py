99edba3408949b4eb70142807c36541d27efa218
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-04 19:42:33 +0000

    problems 0 to 3

---
+++ 2.py
@@ -0,0 +1,15 @@
+
+def fib():
+    x1 = 1
+    x2 = 2
+    while True:
+        x1, x2 = x2, (nxt := x1 + x2)
+        yield nxt
+
+s = 0
+for i in fib():
+    if i > 4_000_000:
+        break
+    if i % 2 == 0:
+        s += i
+print(2+s)  # 4613732
---
+++ 3.py
@@ -0,0 +1,8 @@
+
+def find_prime_factors(n: int) -> set[int]:
+    for i in range(2, int(n**0.5)):
+        if n % i == 0:
+            return find_prime_factors(i) | find_prime_factors(n//i)
+    return {n}
+
+print(max(find_prime_factors(600851475143)))  # 6857
