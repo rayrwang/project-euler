8ad1307e031da50bb983c5a8e87616f98bc0d7a1
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-05 01:03:57 +0000

    p 357

---
+++ p0357.py
@@ -0,0 +1,25 @@
+
+import numba
+
+@numba.jit
+def is_prime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+@numba.jit
+def calc_sum():
+    s = 0
+    for n in range(2, 100_000_000, 2):
+        for i in range(1, int(n**0.5)+1):
+            if n % i == 0:
+                if not is_prime(i + n//i):
+                    break
+        else:
+            s += n
+    return s
+
+if __name__ == "__main__":
+    print(1+calc_sum())  # 1739023853137
+    # TODO make faster
