348bc11199f5b32dea93ae11b2a3e13f036f0e5f
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-05 09:02:32 +0000

    p 357 faster prime calculation

--- p0357.py
+++ p0357.py
@@ -3,8 +3,18 @@ import numba
 
 @numba.jit
 def is_prime(n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
+    if n == 2 or n == 5 or n == 11:
+        return True
+    if n % 2 == 0 or n % 5 == 0:
+        return False
+    for i in range(3, int(n**0.5)+1, 10):
+        if n % i == 0:      # 10k + 3
+            return False
+        if n % (i+4) == 0:  # 10k + 7
+            return False
+        if n % (i+6) == 0:  # 10k + 9
+            return False
+        if n % (i+8) == 0:  # 10k + 1
             return False
     return True
 
@@ -22,4 +32,3 @@ def calc_sum():
 
 if __name__ == "__main__":
     print(1+calc_sum())  # 1739023853137
-    # TODO make faster
