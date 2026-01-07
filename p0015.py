b0e90b8daabe4efdd9f321acdaf1df12a735a861
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-07 10:29:56 +0000

    p 15

---
+++ p0015.py
@@ -0,0 +1,17 @@
+
+def fact(n):
+    prod = 1
+    for i in range(2, n+1):
+        prod *= i
+    return prod
+
+def nCr(n, r):
+    # n! / (r!(n-r)!)
+    prod = 1
+    for i in range(max(r, n-r)+1, n+1):
+        prod *= i
+    return int(prod / fact(min(r, n-r)))
+
+if __name__ == "__main__":
+    # print(int(fact(40) / (fact(20)*fact(20))))  # 137846528820
+    print(nCr(40, 20))  # # 137846528820
