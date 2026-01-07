f26b363429f68ee3ba8352127b370c34b44aa36a
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-07 15:27:54 +0000

    update testing

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ project-euler/
 |-- funcs.py  # Functions used across solutions
 |-- pXXXX.py  # Solution(s) for Problem XXXX
 |-- ...
-|-- test.py   # Run, check, and print execution times for all solutions
+|-- test.py   # Run and print execution times for all solutions
 ```
--- test.py
+++ test.py
@@ -5,10 +5,13 @@ import sys
 import time
 
 if __name__ == "__main__":
+    solutions = 0
+    t_start_all = time.perf_counter()
     for name in sorted(os.listdir()):
         if name.startswith("p"):
+            solutions += 1
             print(f"\nProblem {int(name[1:5])}:")
-            start = time.perf_counter()
+            t_start_solution = time.perf_counter()
             subprocess.run([sys.executable or "python", name])
-            print(f"(in {time.perf_counter()-start:.1f}s)")
-
+            print(f"(in {time.perf_counter()-t_start_solution:.1f}s)")
+    print(f"Ran {solutions} solutions in {time.perf_counter()-t_start_all:.1f}s.")
