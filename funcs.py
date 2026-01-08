b9ddc5e004d2eeb03452b3b44347ba534ddf057d
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-08 13:21:03 +0000

    faster is_prime, add is_square and gcd

--- funcs.py
+++ funcs.py
@@ -3,17 +3,28 @@ import numba
 
 @numba.jit
 def is_prime(n):
-    if n == 2 or n == 5 or n == 11:
+    if n == 2 or n == 3 or n == 5:
         return True
-    if n % 2 == 0 or n % 5 == 0:
+    if n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
         return False
-    for i in range(3, int(n**0.5)+1, 10):
-        if n % i == 0:      # 10k + 3
+    for i in range(7, int(n**0.5)+1, 6):
+        if n % i == 0:  # 6k + 1
             return False
-        if n % (i+4) == 0:  # 10k + 7
-            return False
-        if n % (i+6) == 0:  # 10k + 9
-            return False
-        if n % (i+8) == 0:  # 10k + 1
+        elif n % (i+4) == 0:  # 6k + 5
             return False
     return True
+
+@numba.jit
+def is_square(n):
+    return (int(n**0.5))**2 == n
+
+@numba.jit
+def gcd(x, y):
+    """Greatest common divisor using Euclid's algorithm"""
+    a = max(x, y)
+    b = min(x, y)
+    rem = a % b
+    while rem != 0:
+        a, b = b, rem
+        rem = a % b
+    return b
