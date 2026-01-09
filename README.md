27aff70e83f8290c6439fc78b61494bbac40b3ec
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-09 06:57:07 +0000

    add solution checking

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ project-euler/
 |-- funcs.py  # Functions used across solutions
 |-- pXXXX.py  # Solution(s) for Problem XXXX
 |-- ...
-|-- test.py   # Run and print execution times for all solutions
+|-- test.py   # Run and check all solutions
 ```
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import time
+import re
 
 if __name__ == "__main__":
     solutions = 0
@@ -12,6 +13,28 @@ if __name__ == "__main__":
             solutions += 1
             print(f"\nProblem {int(name[1:5])}:")
             t_start_solution = time.perf_counter()
-            subprocess.run([sys.executable or "python", name])
+            proc = subprocess.run([sys.executable or "python", name], capture_output=True)
+            answer = proc.stdout.decode().strip()
+
+            # Check answer:
+            # The solution is written as a comment on the line with a print statement
+            solution = None
+            solution_pattern = "print\(.+#"
+            with open(name, "r") as f:
+                for line in f:
+                    if re.search(solution_pattern, line):
+                        split = re.split(solution_pattern, line)
+                        if split:
+                            solution = split[-1].strip()
+                            break
+            if solution:
+                if answer == solution:
+                    print(f"{answer} ✔️")
+                else:
+                    print(f"{answer} ❌ (solution: {solution})")
+            else:
+                print(f"{answer} ? (no solution found)")
+
             print(f"(in {time.perf_counter()-t_start_solution:.1f}s)")
+            
     print(f"Ran {solutions} solutions in {time.perf_counter()-t_start_all:.1f}s.")
