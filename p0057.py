bbb219448fd1204aada51ad81ac7229c5882f86f
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-10 13:19:44 +0000

    p 57

---
+++ p0057.py
@@ -0,0 +1,21 @@
+
+import math
+
+def count_digits(n):
+    return int(math.log(n) / math.log(10)) + 1
+
+if __name__ == "__main__":
+    # Starting term is 1 + 1/2
+    num_recurse = 1
+    denom_recurse = 2
+
+    more_digits = 0
+    for _ in range(1000):
+        # 1 + num_recurse/denom_recurse
+        num = num_recurse + denom_recurse
+        denom = denom_recurse
+        if count_digits(num) > count_digits(denom):
+            more_digits += 1
+        # new = 1 / (2 + old)
+        num_recurse, denom_recurse = denom_recurse, (2*denom_recurse + num_recurse)
+    print(more_digits)  # 153
