b5c87e092be7296dd830db6f6ae1c9cd20da6631
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-11 13:28:42 +0000

    p 116

---
+++ p0116.py
@@ -0,0 +1,18 @@
+
+import numba
+
+@numba.jit
+def count_tiles(n, size):
+    """For n contiguous squares"""
+    if n < size:
+        return 0
+    if n == size:
+        return 1
+    count = 0
+    for start in range(n-size+1):
+        count += 1 + count_tiles(n-size-start, size)
+    return count
+
+if __name__ == "__main__":
+    print(count_tiles(50, 2) + count_tiles(50, 3) + count_tiles(50, 4))  # 20492570929
+    # TODO inefficient
