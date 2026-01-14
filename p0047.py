c58f1bf18494f185e7515572c6862bc46258d468
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-14 17:29:56 +0000

    p 47

---
+++ p0047.py
@@ -0,0 +1,13 @@
+
+from funcs import find_prime_factors
+
+if __name__ == "__main__":
+    consecutive = 0
+    for n in range(1, 1<<62):
+        if len(set(find_prime_factors(n))) == 4:
+            consecutive += 1
+        else:
+            consecutive = 0
+        if consecutive == 4:
+            break
+    print(n-3)  # 134043
