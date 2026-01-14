62a7e443a26a5c2ac6b110ee479a5bc8716030ef
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-14 10:14:37 +0000

    explanation for p 932

--- p0932.py
+++ p0932.py
@@ -1,4 +1,16 @@
 
+"""typst
+= Problem 932: $2025$
+
+Let $d$ be the number of digits in $b$. We have:
+
+$
+10^d a + b = (a+b)^2 \
+b^2 + (2a - 1)b + a^2 - 10^d a = 0 \
+b = (1 - 2a + sqrt(4 dot 10^d a - 4a + 1)) / 2
+$
+"""
+
 import numba
 
 from funcs import is_square
@@ -10,6 +22,7 @@ def T(max_digits):
         # longer_len = max_digits - shorter_len
         for n in range(int(10**(shorter_len-1)), int(10**shorter_len)):
             for longer_len in range(1, max_digits - shorter_len + 1):
+                # Case 1
                 # n is a (shorter), find b (longer)
                 a = n
                 disc = 4*10**longer_len*a - 4*a + 1
@@ -18,9 +31,9 @@ def T(max_digits):
                     if b > 0 and len(str(b)) == longer_len:
                         ab = a*10**longer_len + b
                         if ab == (a+b)**2:
-                            print(ab, a, b)
                             s += ab
 
+                # Case 2: (happens to be unnecessary)
                 # n is b (shorter), find a (longer)
                 b = n
                 disc = 10**(2*shorter_len) - 4*10**shorter_len*b - 4*b
@@ -29,7 +42,6 @@ def T(max_digits):
                     if a > 0 and len(str(a)) == longer_len:
                         ab = a*10**shorter_len + b
                         if ab == (a+b)**2:
-                            print(ab, a, b)
                             s += ab
     return s
 
