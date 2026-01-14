8ae37a4d20cfd6c35fd7849258674ecfbe08b0cb
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-14 14:57:28 +0000

    p 18

---
+++ p0018.py
@@ -0,0 +1,37 @@
+
+triangle_raw = """
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
+
+if __name__ == "__main__":
+    triangle = []
+    triangle_max = []
+    # Parse
+    for row_raw in triangle_raw.split("\n")[1:-1]:
+        row = [int(digit) for digit in row_raw.split(" ")]
+        triangle.append(row)
+        triangle_max.append([None]*len(row))
+
+    # Init bottom row of triangle_max to triangle
+    for i, num in enumerate(triangle[-1]):
+        triangle_max[-1][i] = num
+
+    # Work from bottom to top
+    for i in range(len(triangle)-2, 0-1, -1):  # Second row from bottom, to top
+        for j in range(len(triangle[i])):
+            triangle_max[i][j] = triangle[i][j] + max(triangle_max[i+1][j], triangle_max[i+1][j+1])
+    print(triangle_max[0][0])  # 1074
