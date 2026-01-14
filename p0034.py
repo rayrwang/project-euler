49ad5019c7e6e420dc23d4583fdb60804db0de68
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-14 08:03:17 +0000

    speedup p 34

--- p0034.py
+++ p0034.py
@@ -1,12 +1,23 @@
 
-from funcs import fact
+import numba
 
-def sum_fact_digits():
+from funcs import fact_bounded as fact
+
+@numba.jit
+def sum_fact_digits(n):
+    s = 0
+    while n != 0:
+        s += fact(n % 10)
+        n //= 10
+    return s
+
+@numba.jit
+def sum_sfd():
     s = 0
     for i in range(3, 7*fact(9)):  # 8 digits is too long
-        if sum([fact(int(digit)) for digit in str(i)]) == i:
+        if sum_fact_digits(i) == i:
             s += i
     return s
 
 if __name__ == "__main__":
-    print(sum_fact_digits())  # 40730
+    print(sum_sfd())  # 40730
