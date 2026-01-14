755e2f6e46f806161a986b60cd4d036eb8a154de
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-14 07:48:55 +0000

    speedup p 23

--- p0023.py
+++ p0023.py
@@ -1,15 +1,22 @@
 
+import numba
+
 from funcs import sum_proper_divisors
 
+@numba.jit
 def is_abundant(n):
     return sum_proper_divisors(n) > n
 
-if __name__ == "__main__":
+@numba.jit
+def count_non_abundant():
     s = 0
-    for n in range(28123):
-        for i in range(n):
+    for n in range(28123+1):
+        for i in range(n//2 + 1):
             if is_abundant(i) and is_abundant(n-i):
                 break
         else:
             s += n
-    print(s)  # 4179871
+    return s
+
+if __name__ == "__main__":
+    print(count_non_abundant())  # 4179871
