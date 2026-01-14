10088c39b748351b9803f5f391990d3f74f67e31
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-14 16:05:26 +0000

    p 49

---
+++ p0049.py
@@ -0,0 +1,18 @@
+
+from itertools import permutations
+
+from funcs import is_prime
+
+if __name__ == "__main__":
+    for n in range(1000, 10_000-2*3330):
+        if n != 1487:
+            n2 = n + 3330
+            n3 = n + 2*3330
+            str_n = str(n)
+            str_n2 = str(n2)
+            str_n3 = str(n3)
+            if is_prime(n) and is_prime(n2) and is_prime(n3):
+                if sorted(str_n) == sorted(str_n2) == sorted(str_n3):
+                    concat = str_n + str_n2 + str_n3
+                    break
+    print(concat)  # 296962999629
