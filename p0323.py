7f8c24e798372ad20fbef5f1b53eb618ef5cd669
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-15 09:38:06 +0000

    p 323

---
+++ p0323.py
@@ -0,0 +1,17 @@
+
+def cdf(i):
+    """Probability that the sequence will be all one before term i"""
+    return ((2**i-1)/(2**i))**32
+
+def p(i):
+    """Probability that the sequence will become all one on exactly term i"""
+    return cdf(i) - cdf(i-1)
+
+def expectation(terms):
+    E = 0
+    for i in range(terms):
+        E += i * p(i)
+    return E
+
+if __name__ == "__main__":
+    print(expectation(10000))  # 6.3551758451
