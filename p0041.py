a1fcae44765e07147b17c4fce85596b796e74f0a
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-01-30 16:15:27 +0000

    Remove unnecessary tuple()

--- p0041.py
+++ p0041.py
@@ -6,7 +6,7 @@ from funcs import is_prime
 if __name__ == "__main__":
     largest = 0
     for n in range(1, 9+1):
-        for num in itertools.permutations(tuple(str(i) for i in range(1, n+1))):
+        for num in itertools.permutations(str(i) for i in range(1, n+1)):
             # print(num)
             num = int("".join(num))
             if is_prime(num) and num > largest:
