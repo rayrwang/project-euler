6cde593b2691f2b45ff5d71d7313ce1edf60e4b1
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-02-19 05:36:16 +0000

    p 124

---
+++ p0124.py
@@ -0,0 +1,15 @@
+
+import math
+
+from funcs import find_prime_factors_set
+
+def rad(n):
+    return math.prod(find_prime_factors_set(n))
+
+if __name__ == "__main__":
+    rads = {}
+    for n in range(1, 100_000+1):
+        rads[n] = rad(n)
+    print(rads[100])
+    rads = sorted(rads.items(), key=lambda item: item[1])
+    print(rads[10_000-1][0])  # 21417
