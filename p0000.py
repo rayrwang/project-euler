73279fee8856632c5777f72bcb364ee33509dedf
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-02-23 05:07:45 +0000

    p 0 use constant

--- p0000.py
+++ p0000.py
@@ -1,6 +1,7 @@
 
 if __name__ == "__main__":
     s = 0
-    for i in range(1, 213_000, 2):
+    FIRST_N_SQUARES = 213_000
+    for i in range(1, FIRST_N_SQUARES, 2):
         s += i**2
     print(s)  # 1610599499964500
