525ee200376e8d4aaf315a25c3812a2d93c4cd9d
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-06-05 15:22:09 +0100

    P 35 use prime sieve

--- p0035.py
+++ p0035.py
@@ -1,5 +1,5 @@
 
-from funcs import is_prime
+from funcs import prime_sieve_bool
 
 def rot(s: str):
     for _ in range(len(s)):
@@ -7,10 +7,11 @@ def rot(s: str):
         s = s[1:] + s[0]
 
 if __name__ == "__main__":
+    primes = prime_sieve_bool(10_000_000)
     s = 0
     for n in range(2, 1_000_000):
         for i in rot(str(n)):
-            if not is_prime(int(i)):
+            if not primes[int(i)]:
                 break
         else:
             s += 1
