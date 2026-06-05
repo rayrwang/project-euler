7144e8d3bd478acef20b2cb3e1ce9eed10d42d56
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-06-05 15:13:07 +0100

    P 10 use sieve

--- p0010.py
+++ p0010.py
@@ -1,9 +1,13 @@
 
-from funcs import is_prime
+import numpy as np
 
 if __name__ == "__main__":
-    s = 0
-    for n in range(3, 2_000_000, 2):
-        if is_prime(n):
-            s += n
-    print(2+s)  # 142913828922
+    is_prime_array = np.full(2_000_000, True)
+    is_prime_array[0] = False
+    is_prime_array[1] = False
+    for n in range(2, int(2_000_000**0.5)+1):
+        if is_prime_array[n]:  # Not already done by lower numbers
+            is_prime_array[n**2::n] = False
+
+    # Sum primes
+    print(np.sum(np.arange(2_000_000, dtype=np.int64)[is_prime_array]))  # 142913828922
