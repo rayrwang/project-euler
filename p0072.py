c1b5946d286c7579b0ab2976b01d99df9992acb1
Raymond Wang <98355194+rayrwang@users.noreply.github.com>
2026-06-09 06:34:23 +0100

    P 72

--- p0072.py
+++ p0072.py
@@ -1,33 +1,9 @@
-
-import numba
-
-from funcs import find_prime_factors_set, lcm
-
-@numba.jit
-def count_fractions():
-    count = 0
-    for d in range(2, 1_000_000+1):
-        prime_factors = sorted(find_prime_factors_set(d))
-        if len(prime_factors) == 1:
-            possibilities = (d-1) - (d-1)//(prime_factors[0])
-        elif len(prime_factors) == 2:
-            possibilities = (d-1) \
-                - (d-1)//prime_factors[0] \
-                - (d-1)//prime_factors[1] \
-                + (d-1)//lcm(prime_factors[0], prime_factors[1])
-        else:  # TODO better way?
-            possibilities = (d-1) \
-                - (d-1)//prime_factors[0] \
-                - (d-1)//prime_factors[1] \
-                + (d-1)//lcm(prime_factors[0], prime_factors[1])
-            multiples = set()
-            for f in prime_factors[2:]:
-                for n in range(f, d, f):
-                    if n % prime_factors[0] != 0 and n % prime_factors[1] != 0:
-                        multiples.add(n)
-            possibilities -= len(multiples)
-        count += possibilities
-    return count
+from funcs import totient_sieve
 
 if __name__ == "__main__":
-    print(count_fractions())  # 303963552391
+    N = 1_000_000
+    # A reduced proper fraction n/d (n < d, gcd(n, d) = 1) exists for each n
+    # coprime to d, so denominator d contributes exactly phi(d) fractions.
+    # The answer is therefore sum_{d=2}^{N} phi(d).
+    phi = totient_sieve(N + 1)
+    print(int(phi[2:].sum()))  # 303963552391
